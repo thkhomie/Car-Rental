3d4bcf9fe5bd87c27b46eafec21ed557d3b5caed
thkhomie <prajal.gurung17@gmail.com>
2024-04-22 10:19:21 +0545

    GOOD

--- car/src/Parallax/Banner.jsx
+++ car/src/Parallax/Banner.jsx
@@ -1,9 +1,12 @@
+import parallaxImage from '../assets/LR.png';
+
 export default function Banner(){
     return(
-        <div className="flex items-center justify-center w-full h-56 md:h-72 bg-fixed bg-parallax bg-cover">
-            <h1 className="text-2xl md:text-5xl font-semibold text-center text-[#FFF3B0] uppercase ">"Drive  Explore  Enjoy!"</h1>
-        </div>
-
-        
+        <div 
+            className="flex items-center justify-center w-full h-56 md:h-72 bg-fixed bg-cover"
+            style={{backgroundImage: `url(${parallaxImage})`}}
+        >
+            <h1 className="text-2xl md:text-5xl font-semibold text-center text-[#FFF3B0] uppercase">"Drive  Explore  Enjoy!"</h1>
+        </div>  
     )
 }
--- car/tailwind.config.js
+++ car/tailwind.config.js
@@ -6,20 +6,20 @@ export default {
   ],
   theme: {
     extend: {
-      colors:{
-        overAll : '#ffef9f',
-       sectionBG: '#f2e8cf',
-       heading : '#bc4749',
-       paragraph : '#ebf2fa',
-       darkBlue: "#1B4282",
-       lightBlue: "#BDD6FF",
-       overlayBg: "rgba(0,0,0,0.3)"
+      colors: {
+        overAll: '#ffef9f',
+        sectionBG: '#f2e8cf',
+        heading: '#bc4749',
+        paragraph: '#ebf2fa',
+        darkBlue: "#1B4282",
+        lightBlue: "#BDD6FF",
+        overlayBg: "rgba(0,0,0,0.3)"
       },
       backgroundImage: {
-      'parallax' : 'url("./src/assets/LR.png")'
-    }
+        'parallax': 'url("./src/assets/LR.png")'
+      }
     },
-    
+
   },
   plugins: [],
 }
